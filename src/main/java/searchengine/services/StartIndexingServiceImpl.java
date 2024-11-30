de8f126d5a30fbf443ce8465ebac76971d3c5b70
selden39 <selden39@gmail.com>
2024-11-30 13:21:00 +0300

    GET /api/startIndexing: промежуточная версия сервиса: 1. рефакторинг - выделение методов

--- src/main/java/searchengine/services/StartIndexingServiceImpl.java
+++ src/main/java/searchengine/services/StartIndexingServiceImpl.java
@@ -3,9 +3,9 @@ package searchengine.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import searchengine.config.RequestParameters;
+import searchengine.config.Site;
 import searchengine.config.SitesList;
 import searchengine.dto.statistics.StartIndexingResponse;
-import searchengine.model.Site;
 import searchengine.model.Status;
 import searchengine.repositories.PageRepository;
 import searchengine.repositories.SiteRepository;
@@ -31,33 +31,14 @@ public class StartIndexingServiceImpl implements StartIndexingService{
         StartIndexingResponse startIndexingResponse =
                 new StartIndexingResponse(true);
 
-    // 0. Отладочная информация
-        System.out.println("=== print table before indexing ===");
-        Collection<Site> currentSites = siteRepository.findAll();
-        currentSites.forEach(siteX -> {
-            System.out.println(siteX.getId() + " - " + siteX.getName() + " - " + siteX.getUrl());
-        });
+        printDebugInfo();
+        clearTables();
 
-        System.out.println("=== print configured sites ===");
-        sites.getSites().forEach(site -> {
-            System.out.println(site.getName() + " :  " + site.getUrl());
-        });
-
-    // 1. очистить таблицы site and page
-        siteRepository.deleteAll();
-        pageRepository.deleteAll();
-
-    // 2. заполнение таблиц с использованием ForkJoinPool
         //TODO обработку каждого сайта нужно запустить в отдельном потоке
-
         sites.getSites().forEach(siteFromConfig -> {
             System.out.println("=== SITE ===");
-            Site site = new Site();
-            site.setStatus(Status.INDEXING);
-            site.setStatusTime(LocalDateTime.now());
-            site.setUrl(siteFromConfig.getUrl());
-            site.setName(siteFromConfig.getName());
-            siteRepository.save(site);
+            searchengine.model.Site site = new searchengine.model.Site();
+            saveSitesData(siteFromConfig, site);
             List<String> siteLinks = getUrlList(site);
             siteLinks.forEach(System.out::println);
         });
@@ -65,7 +46,34 @@ public class StartIndexingServiceImpl implements StartIndexingService{
         return startIndexingResponse;
     }
 
-    public List<String> getUrlList (Site site){
+    public void printDebugInfo(){
+        System.out.println("=== print table before indexing ===");
+        Collection<searchengine.model.Site> currentSites = siteRepository.findAll();
+        currentSites.forEach(siteX -> {
+            System.out.println(siteX.getId() + " - " + siteX.getName() + " - " + siteX.getUrl());
+        });
+
+        System.out.println("=== print configured sites ===");
+        sites.getSites().forEach(siteFromConfig -> {
+            System.out.println(siteFromConfig.getName() + " :  " + siteFromConfig.getUrl());
+        });
+    }
+
+    public void clearTables(){
+        siteRepository.deleteAll();
+        pageRepository.deleteAll();
+    }
+
+    public void saveSitesData(Site siteFromConfig, searchengine.model.Site site){
+            System.out.println("=== SITE: " + siteFromConfig.getUrl() + " ===");
+            site.setStatus(Status.INDEXING);
+            site.setStatusTime(LocalDateTime.now());
+            site.setUrl(siteFromConfig.getUrl());
+            site.setName(siteFromConfig.getName());
+            siteRepository.save(site);
+    }
+
+    public List<String> getUrlList (searchengine.model.Site site){
         WebPage rootWebPage = new WebPage(site, pageRepository, siteRepository, requestParameters);
         List<String> urlList = new ForkJoinPool()
                 .invoke(new SiteMapCompiler(rootWebPage, pageRepository, siteRepository, requestParameters));
