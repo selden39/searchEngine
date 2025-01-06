96c7846d0ff77ca32d11bf3c6071fa460003b5c5
selden39 <selden39@gmail.com>
2025-01-06 23:14:16 +0300

    GET /api/stopIndexing: убрал попытку инерраптить инициирующие потоки - толку от этого мало. + корректный вывод ответного сообщения на выхов метода

--- src/main/java/searchengine/services/StopIndexingServiceImpl.java
+++ src/main/java/searchengine/services/StopIndexingServiceImpl.java
@@ -4,34 +4,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import searchengine.dto.statistics.OperationIndexingResponse;
 import searchengine.services.indexingexecutor.ThreadCollector;
-import searchengine.services.indexinginterrupter.IndexingInterrupter;
-
-import java.time.LocalDateTime;
 
 
 @RequiredArgsConstructor
 @Service
 public class StopIndexingServiceImpl implements StopIndexingService{
+    private final String UNEXPECTED_ERROR_DESC = "Непредвиденная ошибка";
+    private final String INDEXING_NOT_RUN__ERROR_DESC = "Индексация не запущена";
 
     @Override
     public OperationIndexingResponse getStopIndexing(){
         OperationIndexingResponse operationIndexingResponse;
 
-        ThreadCollector.getIndexingThreads().forEach((thread, forkJoinPool) -> {
-            Thread interrupter = new Thread(new IndexingInterrupter(thread));
-            interrupter.start();
-            System.out.println("+++++++++++++++++");
-            System.out.println(thread.getName() + " - " + thread.isInterrupted());
-            System.out.println(forkJoinPool + ": " + forkJoinPool.getRunningThreadCount());
-            System.out.println(forkJoinPool.isShutdown());
-            forkJoinPool.shutdownNow();
-            System.out.println(forkJoinPool.isShutdown());
-            System.out.println("------------------");
-            System.out.println(thread.isInterrupted());
-        });
-
-        LocalDateTime now = LocalDateTime.now();
-        operationIndexingResponse = new OperationIndexingResponse(true, now.toString());
+        if (ThreadCollector.getIndexingThreads().isEmpty()){
+            operationIndexingResponse = new OperationIndexingResponse(false, INDEXING_NOT_RUN__ERROR_DESC);
+        } else {
+            try {
+                ThreadCollector.getIndexingThreads().forEach((thread, forkJoinPool) -> {
+                    forkJoinPool.shutdownNow();
+                });
+                operationIndexingResponse = new OperationIndexingResponse(true);
+            } catch (Exception e) {
+                operationIndexingResponse = new OperationIndexingResponse(false, UNEXPECTED_ERROR_DESC);
+            }
+        }
         return operationIndexingResponse;
     }
 }
